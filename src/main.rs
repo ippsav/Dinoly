a00bae2d9a71751dcf3f016931de4bb80a06de52
ippsav <mehdiboujid@gmail.com>
2022-11-14 21:09:23 +0000

    axum boilerplate + health check route added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Json, Router};
+use serde_json::{json, Value};
+
+async fn health_check_handler() -> Json<Value> {
+    Json(json!({
+        "status": "ok"
+    }))
+}
+
+#[tokio::main]
+async fn main() {
+    // Create axum router
+    let app = Router::new().route("/health_check", get(health_check_handler));
+
+    // Start server
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
