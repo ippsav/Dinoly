8ee0885abaa91259e33690e80ff3b1784c62cb1a
ippsav <mehdiboujid@gmail.com>
2022-11-16 21:21:24 +0000

    adding first test for health_check route

---
+++ tests/api/health_check.rs
@@ -0,0 +1,39 @@
+use hyper::{Body, Method, Request};
+use serde_json::{json, Value};
+
+use crate::helpers::{server::TestApp, ParseJson};
+
+#[tokio::test]
+async fn health_route_status() {
+    // Run server
+    let mut app = TestApp::new();
+    app.spawn_server().await;
+
+    // Create client
+    let client = hyper::Client::new();
+
+    // Create request
+    let request = Request::builder()
+        .method(Method::GET)
+        .uri(app.get_http_uri(Some("/health_check")))
+        .body(Body::empty())
+        .expect("could not make request");
+    // Send request
+    let response = client
+        .request(request)
+        .await
+        .expect("couldn't send request");
+
+    let status = response.status();
+    assert!(status.is_success());
+
+    let body: Value = response
+        .json_from_body()
+        .await
+        .expect("couldn't get json from body");
+    let expected_body = json!({
+        "status":"ok"
+    });
+
+    assert_eq!(expected_body, body);
+}
