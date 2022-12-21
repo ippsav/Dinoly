79cb84d4f45f32936341377856f31e18cba42108
ippsav <mehdiboujid@gmail.com>
2022-12-21 21:16:27 +0000

    refactoring api response implementation to use Result enum

--- src/handler/helpers/api_response.rs
+++ src/handler/helpers/api_response.rs
@@ -3,7 +3,25 @@ use serde::Serialize;
 
 use super::{ApiResponseError, ApiResponseErrorObject};
 
-pub enum ApiResponse<T: Serialize> {
+pub trait ErrorToResponse {
+    fn into_api_response<T: Serialize ,E: Serialize>(self) -> ApiResponse<T,E>;
+}
+
+// Response types
+pub enum ApiResponseType {
+    SuccessWithData,
+    StatusCodeOnly,
+    Error,
+}
+
+impl Default for ApiResponseType {
+    fn default() -> Self {
+        Self::SuccessWithData
+    }
+}
+
+
+pub enum ApiResponseData<T:Serialize> {
     Data {
         data: T,
         status: StatusCode,
@@ -15,8 +33,62 @@ pub enum ApiResponse<T: Serialize> {
     StatusCode(StatusCode),
 }
 
-pub trait ErrorToResponse {
-    fn into_api_response<T: Serialize>(self) -> ApiResponse<T>;
+impl<T> ApiResponseData<T> 
+where
+    T:Serialize + 'static
+{
+    pub fn success_with_data(data: T, status:StatusCode) -> Self {
+        Self::Data {
+            data,
+            status,
+        }
+    }
+
+    pub fn status_code(status:StatusCode) -> Self {
+        Self::StatusCode(status)
+    }
+
+    pub fn error(error: Option<T>, message:&'static str, status:StatusCode) -> Self {
+        match error {
+            Some(err) => Self::Error {
+                    error: ApiResponseError::complicated_error(message, err),
+                    status,
+            },
+            None => Self::Error {
+                error: ApiResponseError::simple_error(message),
+                status,
+            },
+        }
+    }
+}
+
+impl<T> IntoResponse for ApiResponseData<T>
+where
+    T:Serialize,
+{
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            ApiResponseData::Data { data, status } => (
+                status,
+                Json(
+                    ApiResponseObject::<T> {
+                        data: Some(data),
+                        error: None,
+                    }
+                )
+            ).into_response(),
+            ApiResponseData::Error { error, status } => (
+                status,
+                Json(
+                    ApiResponseObject::<T> {
+                        data: None,
+                        error: Some(error.into())
+                    }
+                )
+            ).into_response(),
+            ApiResponseData::StatusCode(status) => status.into_response(),
+        }
+    }
 }
 
 // Global api response struct
@@ -29,29 +101,5 @@ where
     error: Option<ApiResponseErrorObject>,
 }
 
-impl<T> IntoResponse for ApiResponse<T>
-where
-    T: Serialize,
-{
-    fn into_response(self) -> axum::response::Response {
-        match self {
-            ApiResponse::Data { data, status } => (
-                status,
-                Json(ApiResponseObject::<T> {
-                    data: Some(data),
-                    error: None,
-                }),
-            )
-                .into_response(),
-            ApiResponse::Error { error, status } => (
-                status,
-                Json(ApiResponseObject::<T> {
-                    data: None,
-                    error: Some(error.into()),
-                }),
-            )
-                .into_response(),
-            ApiResponse::StatusCode(status) => status.into_response(),
-        }
-    }
-}
+pub type ApiResponse<T, E> = Result<ApiResponseData<T>,ApiResponseData<E>>;
+
